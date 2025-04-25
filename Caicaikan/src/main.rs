88f2a5e8941ab67a553e5d2f426e731a27f6080a
BaatarUlaan <baatarulaan931@gmail.com>
2025-04-26 01:55:55 +0200

    在需要时才声明变量，而非C那样在开头就要声明所有需要的变量

--- Caicaikan/src/main.rs
+++ Caicaikan/src/main.rs
@@ -3,28 +3,30 @@ use std::cmp::Ordering;
 use std::io::stdin;
 
 fn main() {
-    let mut guess = String::new();
     let target = rng().random_range(1..100);
     println!("Random number is {}", target);
-
     loop {
         println!("Guess a number!");
-        guess=String::new();
+        let mut guess = String::new();
+        
         stdin().read_line(&mut guess).expect("Failed to read line");
         println!("You guessed: {}", guess);
-        let guess: u32 = match guess.trim().parse(){
+        
+        let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => {
                 println!("Please type a number!");
-                continue
-            },
+                continue;
+            }
         };
-        
 
         match guess.cmp(&target) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {println!("You win!"); break},
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
 }
