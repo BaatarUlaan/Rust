c1e5bed89f826b13c607c577ab4a0df18201f593
BaatarUlaan <baatarulaan931@gmail.com>
2025-04-27 23:01:59 +0200

    Tuple 和 Array

--- DataType/src/main.rs
+++ DataType/src/main.rs
@@ -7,7 +7,16 @@ fn main() {
     
     let u=0x123_222;
     
+    //注意在Rust中使用0o，在C语言中使用0
     let i=0o123;
     let i=0234;
+    
+    
+    let tup:(i32,u32,char)=(-1,2,'A');
+    //一旦创建tuple,就不能变了
+    //tup.0取出第一个元素
+    
+    let array:[i32;4] = [1,2,3,4];
+    let array2=[3;5]; //3重复4次
     
 }
