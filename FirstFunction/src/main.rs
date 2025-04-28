6950132739740c0580ef2adec16a046dc49a7b53
BaatarUlaan <baatarulaan931@gmail.com>
2025-04-28 23:02:03 +0200

    if语句

--- FirstFunction/src/main.rs
+++ FirstFunction/src/main.rs
@@ -14,3 +14,16 @@ fn add_two_nubmer(a:i32,b:i32)->i32{
     //return result;
     5
 }
+
+fn exmaple_for_ic(a:i32){
+    if a>0 {
+        println!("Positive");
+    }
+    else if a<0{
+        println!("Negative");
+    }
+    else{
+        println!("Zero");
+    }
+    
+}
