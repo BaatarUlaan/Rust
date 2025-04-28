f99b23f0e43b397e881d47d68392560c26c3bdf9
BaatarUlaan <baatarulaan931@gmail.com>
2025-04-28 22:50:37 +0200

    函数的参数和返回值

--- FirstFunction/src/main.rs
+++ FirstFunction/src/main.rs
@@ -6,3 +6,9 @@ fn main() {
 fn your_first_function() {
     println!("your first function");
 }
+
+fn add_two_nubmer(a:i32,b:i32)->i32{
+    let result=a+b;
+    println!("a+b={}",result);
+    return result;
+}
