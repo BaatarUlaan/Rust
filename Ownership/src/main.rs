f6c5984d654b44c0f79903b79165d62021f97f4c
BaatarUlaan <baatarulaan931@gmail.com>
2025-04-29 23:07:08 +0200

    一旦有可能发生所有权转移，编译就不会通过（保守检查）

--- Ownership/src/main.rs
+++ Ownership/src/main.rs
@@ -3,6 +3,16 @@ fn main() {
     let s=String::from("Dajiadouzai nali liaotian a?");
     let result=change(s);
     println!("{}",result);
+    
+    
+    
+    let a=String::from("have a try");
+    let mut b=String::from("this is b");
+    const C:bool=false;
+    if C{
+        b=a;
+    }
+    println!("{}",a);
 }
 
 fn change(mut s:String)->String{
