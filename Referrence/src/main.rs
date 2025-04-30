700d84c93e9aef38893d85534838ffe9baadb9b9
BaatarUlaan <baatarulaan931@gmail.com>
2025-05-01 00:09:24 +0200

    由于编译器发现r1的生命周期内r2并未被用到，所以这里可以允许可变引用和只读引用同时存在

--- Referrence/src/main.rs
+++ Referrence/src/main.rs
@@ -2,4 +2,12 @@ fn main() {
     let x=String::from("haha");
     println!("Hello, world! {}",x);
     println!("In x:{}",x);
+
+    let mut x = Box::new(5);
+
+    let r1 = &mut x;
+    println!("{:?}", r1);  // 使用了 r1
+
+    let r2 = &x;
+    println!("{:?}", r2);  // 同时使用了 r2
 }
