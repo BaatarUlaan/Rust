38cba78584cb0a1ebb2d7be7e3315b583034494b
BaatarUlaan <baatarulaan931@gmail.com>
2025-05-01 00:09:59 +0200

    由于编译器发现r1的生命周期内r2并未被用到，所以这里可以允许可变引用和只读引用同时存在
    如果更改成这样，那么编译器就会报错！

--- Referrence/src/main.rs
+++ Referrence/src/main.rs
@@ -6,8 +6,8 @@ fn main() {
     let mut x = Box::new(5);
 
     let r1 = &mut x;
-    println!("{:?}", r1);  // 使用了 r1
 
     let r2 = &x;
     println!("{:?}", r2);  // 同时使用了 r2
+    println!("{:?}", r1);  // 使用了 r1
 }
