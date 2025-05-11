afef20f455995a82b5ff025f3cde7783fca604c4
BaatarUlaan <baatarulaan931@gmail.com>
2025-05-11 23:24:26 +0200

    当使用结构体更新语法..a的时候会发生转移（move）
    针对基本类型，因为它们实现了.copy，所以只是单纯的赋值操作。对于有所有权概念的类型，则是转移了所有权！这会导致原先结构体中的部分不可用！

--- Struct/src/main.rs
+++ Struct/src/main.rs
@@ -1,16 +1,18 @@
 fn main() {
-    let a=create_fstruct(String::from("first account"));
+    let mut a=create_fstruct(String::from("first account"));
     let b =Fstruct{
         username:String::from("second account"),
-        ..a
+        ..a //b 中只有username和a不同
     };
-    
-    
+
+    a.liked_post.push(String::from("abc"));
+    a.id=2;
     
 }
 
 fn create_fstruct(username:String)->Fstruct{
     let r=Fstruct{
+        id:1,
         username,
         friend_list:vec![String::from("IShowSpeed"),String::from("xqc")],
         post:vec![String::from("Hey chat")],
@@ -20,6 +22,7 @@ fn create_fstruct(username:String)->Fstruct{
 }
 
 struct Fstruct{
+    id:usize,
     username:String,
     friend_list:Vec<String>,
     post:Vec<String>,
