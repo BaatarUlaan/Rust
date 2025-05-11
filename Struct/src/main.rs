51ab7f759cd905559e86683f97a1495f32e9f52f
BaatarUlaan <baatarulaan931@gmail.com>
2025-05-11 23:01:17 +0200

    结构体基础用法

---
+++ Struct/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+fn create_fstruct(username:String)->Fstruct{
+    let r=Fstruct{
+        username,
+        friend_list:vec![String::from("IShowSpeed"),String::from("xqc")],
+        post:vec![String::from("Hey chat")],
+        liked_post:vec![String::from("Hey chat")]
+    };
+    r
+}
+
+struct Fstruct{
+    username:String,
+    friend_list:Vec<String>,
+    post:Vec<String>,
+    liked_post:Vec<String>,
+}
