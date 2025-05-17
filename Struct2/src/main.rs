32f876d17eb6ba801bd3ea82f69f567c31f3bf79
BaatarUlaan <baatarulaan931@gmail.com>
2025-05-17 11:39:12 +0200

    勘误：是Copy依赖Clone，而不是Clone包含Copy！

--- Struct2/src/main.rs
+++ Struct2/src/main.rs
@@ -1,9 +1,10 @@
 #[derive(Debug)]
-#[derive(Copy,Clone)]
+#[derive(Clone)] //Copy 依赖Clone
 struct Ren{
     chang:u64,
     gao:u64,
-    zhong:u64
+    zhong:u64,
+    mingzi:String
 }
 
 impl Ren{
@@ -19,7 +20,8 @@ fn main() {
     let mut jia=Ren{
         chang:64,
         gao:64,
-        zhong:64
+        zhong:64,
+        mingzi:String::from("GaGa")
     };
     Ren::zengchangshu(&mut jia);
     println!("{:#?}",jia);
