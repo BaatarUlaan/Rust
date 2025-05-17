c40c7c2fa4c9380388f4bf45b82017d34cb7f51f
BaatarUlaan <baatarulaan931@gmail.com>
2025-05-17 12:41:20 +0200

    由于隐式的生命周期，p.x的可变借用代表了对p整体的可变借用，所以无法对p.y进行不可变借用
    【存疑】

--- Struct2/src/main.rs
+++ Struct2/src/main.rs
@@ -1,18 +1,19 @@
-#[derive(Clone)]
-struct A {
-    x: u32,
-    y:u32,
+struct Point {
+    x: i32,
+    y: i32,
 }
 
-#[derive(Copy, Clone)]
-struct B {
-    x: u32,
+impl Point {
+    fn get_x(&mut self) -> &mut i32 {
+        //fn get_x<'a>(&'a mut self) -> &'a mut i32
+        //注意此处隐式地声明了生命周期
+        &mut self.x
+    }
 }
 
 fn main() {
-    let mut a1 = A { x: 1 , y: 2 };
-    let mut kb=&mut a1.x;
-    *kb+=1;
-    println!("a1.x = {}",kb);
-    
+    let mut p = Point { x: 1, y: 2 };
+    let x = p.get_x();
+    *x += 1;
+    println!("{} {}", *x, p.y);
 }
